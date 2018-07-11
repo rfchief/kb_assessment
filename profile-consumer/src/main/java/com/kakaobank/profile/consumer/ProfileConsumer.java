3006c85a5272dd4d6f23f6fba30b1c7b3087e584
rfchief <rfchief@gmail.com>
2018-07-12 00:32:13 +0900

    * Add packages at profile-consumer project * Add sample apis for retrieving information * Add dependencies for libraries

--- profile-consumer/src/main/java/com/kakaobank/profile/consumer/ProfileConsumer.java
+++ profile-consumer/src/main/java/com/kakaobank/profile/consumer/ProfileConsumer.java
@@ -1,4 +1,18 @@
 package com.kakaobank.profile.consumer;
 
+import static spark.Spark.*;
+
 public class ProfileConsumer {
+    public static void main(String[] args) {
+        get("/api/customer/:customerNumber", (req, res) -> {
+            return "Hello Customer : " + req.params(":customerNumber");
+        });
+
+        get("/api/customer/:customerNumber/account/:accountNumber", (req, res) -> {
+            String customerNumber = req.params(":customerNumber");
+            String accountNumber = req.params(":accountNumber");
+
+            return "Hello Customer '" + customerNumber + "', Account : '" + accountNumber +"'";
+        });
+    }
 }
